85334b882c68da7d2694a612fe14762c1547a4c6
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-12-15 15:04:21 +0300

    PageCommit

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import './App.css';
 import {CommonInput} from "./component/common/Input/CommonInput";
 import {CommonButton} from "./component/common/Button/CommonButton";
 import {CommonCheckbox} from "./component/common/Checkbox/CommonCheckbox";
+import Common from "./component/common/Common";
 
 function App() {
 
 
     return (
     <div className="App">
-
+<Common></Common>
 
     </div>
   );
