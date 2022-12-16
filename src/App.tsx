5b878971f18b4a329a83f52ecaf63f7e309be68f
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-12-16 15:16:28 +0300

    PageRoutesCommonTestPageCommit

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,22 @@ import Profile from "./component/Profile/Profile";
 import ErrorPage from "./component/ErrorPage/ErrorPage";
 import PasswordRecovery from "./component/PasswordRecovery/PasswordRecovery";
 import NewPassword from "./component/ NewPassword/NewPassword";
-import  "./App.css";
+import "./App.css";
 
-export const App=()=> {
+export const App = () => {
     return (
-    <HashRouter>
-        <div className={'App'}>
-        <div>
-            <Common></Common>
-           <Routes>
-               <Route path={'/'}  element={<Navigate to={"/login"}/>}/>
-                <Route path='/login' element={<Login/>}/>
-                <Route path='/registration' element={<Registration/>}/>
-                <Route path='/profile/:userId' element={<Profile/>}/>
-                <Route path='/error404' element={<ErrorPage/>}/>
-                <Route path='/passwordRecovery' element={<PasswordRecovery/>}/>
-                <Route path='/newPassword' element={<NewPassword/>}/></Routes>
-            </div>
-       </div>
-    </HashRouter>
-
-
-
-
-
-  );
-}
-
-export default App;
+        <HashRouter>
+            <div className={'App'}>
+                    <Routes>
+                        <Route path={'/'} element={<Navigate to={"/login"}/>}/>
+                        <Route path='/login' element={<Login/>}/>
+                        <Route path='/registration' element={<Registration/>}/>
+                        <Route path='/profile/:userId' element={<Profile/>}/>
+                        <Route path='/error404' element={<ErrorPage/>}/>
+                        <Route path='/passwordRecovery' element={<PasswordRecovery/>}/>
+                        <Route path='/newPassword' element={<NewPassword/>}/>
+                    <Route path='/testCommon' element={<Common/>}/>
+         </Routes>
+        </div>
+</HashRouter>
+)}
