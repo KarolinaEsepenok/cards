40034bc57cb74aa3e87b3a677e9e3b1ce640e5c9
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-12-23 18:04:38 +0300

    SignInCommit

--- src/features/auth/signIn/signIn.tsx
+++ src/features/auth/signIn/signIn.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 const SignIn = () => {
     return (
         <div>
-           <a> SignIn</a>
+           <h2> SignIn</h2>
+            <div><input/></div>
+            <div><input/></div>
+            <label><input type={"checkbox"}/> remember me</label>
+            <div><NavLink to={'/passwordRecovery'}>Forgot?</NavLink></div>
+            <div><button>Sign In</button></div>
+            <div><NavLink to={'/signUp'}>Sign Up!</NavLink></div>
         </div>
     );
 };
