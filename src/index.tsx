0ac9cf61eec0d2a97b98402ecde1d925a0762efb
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2023-01-09 15:32:39 +0400

    chore: delete unnecessary files

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import { HashRouter } from 'react-router-dom'
 
 import { App } from './app/App'
 import { store } from './app/store'
-import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
@@ -20,8 +19,3 @@ root.render(
     </HashRouter>
   </React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
