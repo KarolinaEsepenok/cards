ded31d8173baaf86581f61ed3ce05bd2c98da955
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2023-01-09 18:24:17 +0400

    fix: fix style in index.scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.scss'
 import { Provider } from 'react-redux'
 import { HashRouter } from 'react-router-dom'
 
