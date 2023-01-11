a481193f8357731581e02aa4920b73398c815d3b
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2023-01-11 20:06:11 +0400

    chore: fix paths hooks

--- src/common/hooks/useAppSelector.ts
+++ src/common/hooks/useAppSelector.ts
@@ -1,5 +1,5 @@
 import { TypedUseSelectorHook, useSelector } from 'react-redux'
 
-import { RootStateType } from '../app/store'
+import { RootStateType } from '../../app/store'
 
 export const useAppSelector: TypedUseSelectorHook<RootStateType> = useSelector
