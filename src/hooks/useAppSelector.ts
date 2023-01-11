f2441943624ebb8bd2f6d6c9ec9aee637a104d3c
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2023-01-11 16:46:56 +0400

    chore: add useAppDispatch and useAppSelector

---
+++ src/hooks/useAppSelector.ts
@@ -0,0 +1,5 @@
+import { TypedUseSelectorHook, useSelector } from 'react-redux'
+
+import { RootStateType } from '../app/store'
+
+export const useAppSelector: TypedUseSelectorHook<RootStateType> = useSelector
