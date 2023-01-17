2cf1f0d70cdbd5ab276de4d1a0c875363163fb22
lily1215z <odessa1215@i.ua>
2023-01-17 14:31:21 +0100

    feat: add getCards thunk, reducer, Api

---
+++ src/features/packs/cards/cardsApi.ts
@@ -0,0 +1,52 @@
+import { AxiosResponse } from 'axios'
+
+import { instance } from '../../../common/axiosInstance/axiosInstance'
+import { sortingCardsMethods } from '../../../common/sortingPacksMethods/sortingPacksMethods'
+
+export const cardsAPI = {
+  getCards(params: GetParamsCardType) {
+    return instance.get<'', AxiosResponse<ResponseGetType>, RequestCardsType>('cards/card', {
+      params: { ...params },
+    })
+  },
+}
+
+//type
+export type GetParamsCardType = {
+  cardsPack_id: string
+  pageCount: number
+  cardQuestion: string
+  page: number
+  sortCards: sortingCardsMethods
+}
+export type CardType = {
+  answer: string
+  question: string
+  cardsPack_id: string
+  grade: number
+  shots: number
+  user_id: string
+  created: string
+  updated: string
+  _id: string
+}
+export type ResponseGetType = {
+  cards: CardType[]
+  cardsTotalCount: number
+  maxGrade: number
+  minGrade: number
+  page: number
+  pageCount: number
+  packUserId: string
+}
+
+export type RequestCardsType = {
+  cardAnswer: string
+  cardQuestion: string
+  cardsPack_id: string
+  min: number
+  max: number
+  sortCards: string
+  page: number
+  pageCount: number
+}
---
+++ src/features/packs/cards/catdsReducer.ts
@@ -0,0 +1,62 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+import { setError, setIsLoading } from '../../../app/app-reducer'
+import { AppThunk } from '../../../common/hooks/AppThunk'
+import { sortingCardsMethods } from '../../../common/sortingPacksMethods/sortingPacksMethods'
+
+import { cardsAPI, CardType } from './cardsApi'
+
+const initialState = {
+  cards: [] as CardType[],
+  cardsTotalCount: 0,
+  packName: '',
+  isCardsFetched: false,
+  queryParams: {
+    pageCount: 5,
+    page: 1,
+    cardQuestion: '',
+    sortCards: sortingCardsMethods.desUpdate,
+  },
+}
+
+export const getCardsTC =
+  (cardsPack_id: string): AppThunk =>
+  async (dispatch, getState) => {
+    dispatch(setIsLoading(true))
+    try {
+      const { pageCount, cardQuestion, page, sortCards } = getState().cards.queryParams
+      const response = await cardsAPI.getCards({
+        cardsPack_id,
+        pageCount,
+        cardQuestion,
+        page,
+        sortCards,
+      })
+      const { cards, cardsTotalCount } = response.data
+
+      dispatch(getCards({ cards: cards }))
+      // dispatch(setCardsTotalCount(cardsTotalCount))
+      // dispatch(setIsCardsFetched(true));
+    } catch (e) {
+      if (axios.isAxiosError<{ error: string }>(e)) {
+        const error = e.response ? e.response.data.error : 'Something wrong'
+
+        dispatch(setError(error))
+      }
+    } finally {
+      dispatch(setIsLoading(false))
+    }
+  }
+const slice = createSlice({
+  name: 'cards',
+  initialState,
+  reducers: {
+    getCards: (state, action: PayloadAction<{ cards: CardType[] }>) => {
+      state.cards = action.payload.cards
+    },
+  },
+})
+
+export const cardsReducer = slice.reducer
+export const { getCards } = slice.actions
