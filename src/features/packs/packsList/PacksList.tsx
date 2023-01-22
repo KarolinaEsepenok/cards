ff993929cb6739d159cbb8ce37c59e4f8b35fc2f
Oleg <9helgi9gmail.com>
2023-01-22 16:48:43 +0100

    feat: sort flow

--- src/features/packs/packsList/PacksList.tsx
+++ src/features/packs/packsList/PacksList.tsx
@@ -1,10 +1,15 @@
 import React, { FC } from 'react'
 
+import arrowDown from '../../../assets/img/icons/arrowDown.png'
+import arrowUp from '../../../assets/img/icons/arrowUp.png'
+import { sortingPacksMethods } from '../../../common/constants/sortingPacksMethods/sortingPacksMethods'
+import { useAppDispatch } from '../../../common/hooks/useAppDispatch'
 import { useAppSelector } from '../../../common/hooks/useAppSelector'
 import { myIdSelector } from '../../../common/selectors/Selectors'
 import list from '../../../common/style/List.module.scss'
 import { formatDate } from '../../../common/utils/formatDate'
 import { PackType } from '../packsApi'
+import { setSort } from '../packsReducer'
 
 import { PackActions } from './pack/actions/PackActions'
 import { Pack } from './pack/Pack'
@@ -15,15 +20,59 @@ type PacksTableType = {
 
 export const PacksList: FC<PacksTableType> = ({ packs }) => {
   const myId = useAppSelector(myIdSelector)
+  const dispatch = useAppDispatch()
+  const sortMethod = useAppSelector(state => state.packs.queryParams.sortPacks)
+  const arrowDirectionName =
+    sortMethod === sortingPacksMethods.desName ? <img src={arrowUp} /> : <img src={arrowDown} />
+  const arrowDirectionCards =
+    sortMethod === sortingPacksMethods.desCardsCount ? <img src={arrowUp} /> : <img src={arrowDown} />
+  const arrowDirectionDate =
+    sortMethod === sortingPacksMethods.desUpdate ? <img src={arrowUp} /> : <img src={arrowDown} />
+  const arrowDirectionCreated =
+    sortMethod === sortingPacksMethods.desUserName ? <img src={arrowUp} /> : <img src={arrowDown} />
+
+  const sortByNameHandler = () => {
+    sortMethod === sortingPacksMethods.desName
+      ? dispatch(setSort(sortingPacksMethods.ascName))
+      : dispatch(setSort(sortingPacksMethods.desName))
+  }
+
+  const sortByDateHandler = () => {
+    sortMethod === sortingPacksMethods.desUpdate
+      ? dispatch(setSort(sortingPacksMethods.ascUpdate))
+      : dispatch(setSort(sortingPacksMethods.desUpdate))
+  }
+
+  const sortByCardsHandler = () => {
+    sortMethod === sortingPacksMethods.desCardsCount
+      ? dispatch(setSort(sortingPacksMethods.ascCardsCount))
+      : dispatch(setSort(sortingPacksMethods.desCardsCount))
+  }
+
+  const sortByCreateHandler = () => {
+    sortMethod === sortingPacksMethods.desUserName
+      ? dispatch(setSort(sortingPacksMethods.ascUserName))
+      : dispatch(setSort(sortingPacksMethods.desUserName))
+  }
 
   return (
     <table className={list.table}>
       <thead>
         <tr>
-          <th className={list.tableTitle}>Name</th>
-          <th className={list.tableTitle}>Cards</th>
-          <th className={list.tableTitle}>Last Updated</th>
-          <th className={list.tableTitle}>Created by</th>
+          <th className={list.tableTitle}>
+            <span onClick={sortByNameHandler}>Name {arrowDirectionName}</span>
+          </th>
+          <th className={list.tableTitle}>
+            <span onClick={sortByCardsHandler}>Cards {arrowDirectionCards}</span>
+          </th>
+          <th className={list.tableTitle}>
+            <span style={{ cursor: 'pointer' }} onClick={sortByDateHandler}>
+              Last Updated {arrowDirectionDate}
+            </span>
+          </th>
+          <th className={list.tableTitle}>
+            <span onClick={sortByCreateHandler}>Created by {arrowDirectionCreated}</span>
+          </th>
           <th className={list.tableTitle}>Actions</th>
         </tr>
       </thead>
