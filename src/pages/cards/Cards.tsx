6d21b3d3edc991b408db643a16c0ca7e916abb86
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2023-01-25 14:55:27 +0400

    feat: add navigate to packs list link in Cards page

--- src/pages/cards/Cards.tsx
+++ src/pages/cards/Cards.tsx
@@ -1,14 +1,16 @@
 import React, { useEffect } from 'react'
 
 import CircularProgress from '@mui/material/CircularProgress'
-import { useParams } from 'react-router-dom'
+import { NavLink, useParams } from 'react-router-dom'
 
 import { useAppDispatch } from 'common/hooks/useAppDispatch'
 import { useAppSelector } from 'common/hooks/useAppSelector'
 import { cardsSelector, isLoadingSelector } from 'common/selectors/Selectors'
 import { CardsList } from 'pages/cards/cardsList/CardsList'
+import s from 'pages/cards/cardsList/learnCard/LearnCard.module.scss'
 import { getCardsTC } from 'pages/cards/cardsSlice'
 import { EmptyPack } from 'pages/packs/packsList/pack/emptyPack/EmptyPack'
+import { PATH } from 'routes/routes'
 
 export const Cards = () => {
   const dispatch = useAppDispatch()
@@ -28,6 +30,10 @@ export const Cards = () => {
 
   return cards.length ? (
     <>
+      <NavLink to={PATH.PACKS} className={s.link}>
+        <p>&lArr; Back to Packs List</p>
+      </NavLink>
+
       <CardsList cards={cards} />
     </>
   ) : (
