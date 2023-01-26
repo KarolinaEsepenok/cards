c00b078bd4090ccdabd3387de2c6baa1da2d20a2
lily1215z <odessa1215@i.ua>
2023-01-26 20:59:22 +0100

    fix: fix modals Styles

--- src/common/components/modals/ModalsButton.tsx
+++ src/common/components/modals/ModalsButton.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 
 import { Button } from 'common/components/button/Button'
-// @ts-ignore
 import s from 'common/components/modals/Modals.module.scss'
 
 type ModalButtonType = {
@@ -9,19 +8,21 @@ type ModalButtonType = {
   typeBtn: ModalButtonVariantType
 }
 export const ModalButton: React.FC<ModalButtonType> = ({ isSaveDataModal, typeBtn }) => {
-  const onKeydown = ({ key }: KeyboardEvent) => {
-    switch (key) {
-      case 'Enter':
-        isSaveDataModal()
-        break
+  if (typeBtn === 'save') {
+    const onKeydown = ({ key }: KeyboardEvent) => {
+      switch (key) {
+        case 'Enter':
+          isSaveDataModal()
+          break
+      }
     }
+
+    useEffect(() => {
+      document.addEventListener('keydown', onKeydown)
+
+      return () => document.removeEventListener('keydown', onKeydown)
+    })
   }
-
-  useEffect(() => {
-    document.addEventListener('keydown', onKeydown)
-
-    return () => document.removeEventListener('keydown', onKeydown)
-  })
 
   return (
     <div>
