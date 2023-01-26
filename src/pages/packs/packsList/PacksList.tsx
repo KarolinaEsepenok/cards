336cf7b8a2cb3816cec22c1fc4341e6b9e795b05
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2023-01-26 13:22:27 +0400

    style: fix styles cards table

--- src/pages/packs/packsList/PacksList.tsx
+++ src/pages/packs/packsList/PacksList.tsx
@@ -2,29 +2,29 @@ import { PackActions } from './pack/actions/PackActions'
 import { Pack } from './pack/Pack'
 import s from './PackList.module.scss'
 
-import arrowDown from 'assets/img/icons/arrowDown.png'
-import arrowUp from 'assets/img/icons/arrowUp.png'
+import arrowDown from 'assets/img/icons/table-sort-arrow-down.svg'
+import arrowUp from 'assets/img/icons/table-sort-arrow-up.svg'
 import { sortingPacksMethods } from 'common/constants/sortingPacksMethods/sortingPacksMethods'
 import { useAppDispatch } from 'common/hooks/useAppDispatch'
 import { useAppSelector } from 'common/hooks/useAppSelector'
-import { cardPacks, myIdSelector } from 'common/selectors/Selectors'
+import { cardPacks, myIdSelector, sortPacksSelector } from 'common/selectors/Selectors'
 import { formatDate } from 'common/utils/formatDate'
 import { sortHelper } from 'common/utils/sortHelper'
 
 export const PacksList = () => {
   const packs = useAppSelector(cardPacks)
   const myId = useAppSelector(myIdSelector)
+  const sortMethod = useAppSelector(sortPacksSelector)
   const dispatch = useAppDispatch()
 
-  const sortMethod = useAppSelector(state => state.packs.queryParams.sortPacks)
   const arrowDirectionName =
-    sortMethod === sortingPacksMethods.ascName ? <img src={arrowUp} /> : <img src={arrowDown} />
+    sortMethod === sortingPacksMethods.ascName ? <img src={arrowUp} alt="" /> : <img src={arrowDown} alt="" />
   const arrowDirectionCards =
-    sortMethod === sortingPacksMethods.ascCardsCount ? <img src={arrowUp} /> : <img src={arrowDown} />
+    sortMethod === sortingPacksMethods.ascCardsCount ? <img src={arrowUp} alt="" /> : <img src={arrowDown} alt="" />
   const arrowDirectionDate =
-    sortMethod === sortingPacksMethods.ascUpdate ? <img src={arrowUp} /> : <img src={arrowDown} />
+    sortMethod === sortingPacksMethods.ascUpdate ? <img src={arrowUp} alt="" /> : <img src={arrowDown} alt="" />
   const arrowDirectionCreated =
-    sortMethod === sortingPacksMethods.ascUserName ? <img src={arrowUp} /> : <img src={arrowDown} />
+    sortMethod === sortingPacksMethods.ascUserName ? <img src={arrowUp} alt="" /> : <img src={arrowDown} alt="" />
 
   const universalSort = (m1: sortingPacksMethods, m2: sortingPacksMethods) => {
     sortHelper(dispatch, sortMethod, m1, m2)
@@ -51,25 +51,25 @@ export const PacksList = () => {
       <thead>
         <tr>
           <th className={`${s.title} ${s.name}`}>
-            <span onClick={sortByName} className={s.cursor}>
+            <span onClick={sortByName} className={s.sort}>
               Name {arrowDirectionName}
             </span>
           </th>
 
           <th className={`${s.title} ${s.cards}`}>
-            <span onClick={sortByCardsCount} className={s.cursor}>
+            <span onClick={sortByCardsCount} className={s.sort}>
               Cards {arrowDirectionCards}
             </span>
           </th>
 
           <th className={`${s.title} ${s.update}`}>
-            <span onClick={sortByUpdate} className={s.cursor}>
+            <span onClick={sortByUpdate} className={s.sort}>
               Last Updated {arrowDirectionDate}
             </span>
           </th>
 
           <th className={`${s.title} ${s.creator}`}>
-            <span onClick={sortByCreatorName} className={s.cursor}>
+            <span onClick={sortByCreatorName} className={s.sort}>
               Created by {arrowDirectionCreated}
             </span>
           </th>
